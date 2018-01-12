29ebbe1931bbfb491c16a3981a7506cf1aa16149
shbhmbnsl1 <shbhmbnsl1@gmail.com>
2018-01-13 01:29:13 +0530

    making single new prediction

--- cnn.py
+++ cnn.py
@@ -55,3 +55,17 @@ test_set = test_datagen.flow_from_directory('dataset/test_set',target_size=(64, 
 
 classifier.fit_generator(training_set,steps_per_epoch=(8000/32),epochs=25,validation_data=test_set,validation_steps=(2000/32)
 
+
+# Making a new prediction
+
+import numpy as np
+from keras.preprocessing import image
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_2.jpg', target_size = (64,64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = classifier.predict(test_image)
+training_set.class_indices
+if result[0][0] == 1:
+  prediction = 'dog'
+else:
+  prediction = 'cat'
