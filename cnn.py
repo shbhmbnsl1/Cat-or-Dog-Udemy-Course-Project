8889019634ee1627b9032cb31772af7aae9d3a93
shbhmbnsl1 <shbhmbnsl1@gmail.com>
2018-01-13 01:27:26 +0530

    adding layers and compiling the cnn

--- cnn.py
+++ cnn.py
@@ -18,3 +18,26 @@ from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
 
+# Initialising the CNN
+classifier = Sequential()
+
+# Step 1 - Convolution
+classifier.add(Convolution2D(32, 3, 3, input_shape = (64, 64, 3), activation = 'relu'))
+
+# Step 2 - Pooling
+classifier.add(MaxPooling2D(pool_size = (2,2)))
+
+
+# Adding a second convoulutional layer for better results
+classifier.add(Convolution2D(32, 3, 3, activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2,2)))
+
+# Step 3 - Flattening
+classifier.add(Flatten())
+
+# Step 4 - Full Connection
+classifier.add(Dense(output_dim = 128, activation = 'relu'))
+classifier.add(Dense(output_dim = 1, activation = 'sigmoid'))
+
+# Compiling the CNN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
