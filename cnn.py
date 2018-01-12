f03fdb8dbbdbe0211235a755ad07b9489431a366
shbhmbnsl1 <shbhmbnsl1@gmail.com>
2018-01-13 00:41:34 +0530

    importing keras libraries

---
+++ cnn.py
@@ -0,0 +1,20 @@
+# Convolutional Neural Network
+
+# Installing Theano
+# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
+
+# Installing Tensorflow
+# pip install tensorflow
+
+# Installing Keras
+# pip install --upgrade keras
+
+# Part 1 - Building the CNN
+
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
